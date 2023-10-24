62a4f0b4da8fc33acefd1b1c6f51ef6a2e9cc500
Harsh dangi <76767370+harsh684@users.noreply.github.com>
2023-10-24 17:26:35 +0530

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -7,7 +7,7 @@ const app= express()
 //transfer the data from frontend to backend in json format
 app.use(express.json())
 app.use(cors({
-    origin: ['https://ecommerce-app-gamma-ashy.vercel.app','https://ecommerce-app-gamma-ashy.vercel.app/signup','https://ecommerce-app-gamma-ashy.vercel.app/login'],
+    origin: ['http://ecommerce-app-gamma-ashy.vercel.app','http://ecommerce-app-gamma-ashy.vercel.app/signup','http://ecommerce-app-gamma-ashy.vercel.app/login'],
     methods: ["POST", "GET"],
     credentials: true
 }));
